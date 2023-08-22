53f37acaf43f3670f6c344b18fe151f19dff42c3
mariethecurie <137413787+mariethecurie@users.noreply.github.com>
2023-08-23 03:35:19 +0400

    Update instruction.h

--- instruction.h
+++ instruction.h
@@ -2,7 +2,6 @@
 #define INSTRUCTION_H
 
 #include <iostream>
-#include <string>
 #include <map>
 
 enum class Register {
@@ -33,53 +32,21 @@ enum class Opcode {
 
 class Instruction {
 public:
-    Instruction () {}
-    ~Instruction() {}
-    void setOpcode (const std::string& opcode) {
-        m_opcode = opcodeValues[opcode];
-    }
-    Opcode opcode () const {
-        return m_opcode;
-    }
-    void setDestReg (const std::string& destReg) {
-        m_destReg = registerValues[destReg];
-    }
-    Register destReg () const {
-        return m_destReg;
-    }
-    void setDestMem (int destMem) {
-        m_destMem = destMem;
-        m_isMem = true;
-    }
-    int destMem () const {
-        return m_destMem;
-    }
-    bool isMem () const {
-        return m_isMem;
-    }
-    void setSrcReg (const std::string& srcReg) {
-        m_srcReg = registerValues[srcReg];
-    }
-    Register srcReg () const {
-        return m_srcReg;
-    }
-    void setImm (int imm) {
-        m_imm = imm;
-        m_isImm = true;
-    }
-    int imm () const {
-        return m_imm;
-    }
-    bool isImm () const {
-        return m_isImm;
-    }
-    void setTarget (int target) {
-        m_target = target;
-    }
-    int target () const {
-        return m_target;
-    }
-    void print() const;
+    Instruction ();
+    void setOpcode (const std::string& opcode);
+    Opcode opcode () const;
+    void setDestReg (const std::string& destReg);
+    Register destReg () const;
+    void setDestMem (int destMem);
+    int destMem () const;
+    bool isMem () const;
+    void setSrcReg (const std::string& srcReg);
+    Register srcReg () const;
+    void setImm (int imm);
+    int imm () const;
+    bool isImm () const;
+    void setTarget (int target);
+    int target () const;
 
 private:
     Opcode m_opcode;
@@ -88,10 +55,10 @@ private:
     int m_destMem;
     bool m_isMem;
     int m_imm;
-    bool m_isImm;
+    bool m_isImm = false;
     int m_target;
-    static std::map<std::string, Opcode> opcodeValues;
-    static std::map<std::string, Register> registerValues;
+    std::map<std::string, Opcode> opcodeValues;
+    std::map<std::string, Register> registerValues;
 };
 
 #endif
